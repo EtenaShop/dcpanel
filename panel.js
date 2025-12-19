6475c9ddf448c0795aff231d0a3e5b6cb6409d84
Baki Kara <mehmeteminacan13@gmail.com>
2025-12-20 01:14:23 +0300

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,5 +1,5 @@
-const API_URL = "https://qualifications-simultaneously-walter-protocol.trycloudflare.com";
-const API_KEY = "SUPER_GIZLI_KEY";
+const API_URL = "https://ricky-briefing-loc-involve.trycloudflare.com";
+const API_KEY = "123";
 
 function kaydet() {
   const prefix = document.getElementById("prefix").value;
